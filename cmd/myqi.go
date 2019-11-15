3b41d7663a1de4782a494d16eef0ae45099eead3
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-11-15 16:09:11 +0900

    golint でエラーが出ない様に修正

--- cmd/myqi.go
+++ cmd/myqi.go
@@ -9,16 +9,16 @@ import (
 
 const baseURL = "https://qiita.com/api/v2/items"
 
-// jsonをパースする為の構造体を定義する
-
+// Data は jsonをパースする為の構造体です
 type Data struct {
 	ID             string `json:"id"`
-	Url            string `json:"url"`
+	URL            string `json:"url"`
 	Title          string `json:"title"`
 	LikesCount     int    `json:"likes_count"`
 	PageViewsCount int    `json:"page_views_count"`
 }
 
+// FetchMyQiitaData は accessToken を使用して qiitaUser の記事一覧を取得します
 func FetchMyQiitaData(accessToken string, qiitaUser string) ([]Data, error) {
 	b, err := json.Marshal(Data{})
 	if err != nil {
@@ -101,7 +101,7 @@ func FetchMyQiitaData(accessToken string, qiitaUser string) ([]Data, error) {
 	return data, nil
 }
 
-// データの出力
+// OutputQiitaData は取得した記事データを表示します
 func OutputQiitaData(data []Data) {
 	fmt.Println("************************自分のQiita投稿一覧******************************")
 	for _, val := range data {
@@ -109,7 +109,7 @@ func OutputQiitaData(data []Data) {
 		fmt.Printf("%-15v%v%v\n", "Title", ": ", val.Title)
 		fmt.Printf("%-12v%v%v\n", "いいね", ": ", val.LikesCount)
 		fmt.Printf("%-9v%v%v\n", "ページビュー", ": ", val.PageViewsCount)
-		fmt.Printf("%-15v%v%v\n", "URL", ": ", val.Url)
+		fmt.Printf("%-15v%v%v\n", "URL", ": ", val.URL)
 		fmt.Println("-------------------------------------------------------------------------")
 	}
 }
